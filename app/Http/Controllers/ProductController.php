736b65ae05c96d673312705535cfdfa5c14fc696
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2024-08-30 17:20:27 +0700

    📝 (ProductController.php): refactor ProductController to use __invoke method instead of index method for better code organization and readability 📝 (routes/web.php): update route definition for '/products' to use ProductController class directly instead of specifying the method name, improving code simplicity and consistency

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __invoke()
     {
         $client = new Client();
 
@@ -21,53 +18,5 @@ class ProductController extends Controller
         $products = json_decode($response->getBody());
 
         return view('welcome', ['products' => $products]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
